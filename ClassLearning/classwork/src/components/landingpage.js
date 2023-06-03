5cfd684b1ef20ab5c150c474860e512af6d037ff
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-06-03 19:08:44 +0530

    Using use state with data in arrays

--- ClassLearning/classwork/src/components/landingpage.js
+++ ClassLearning/classwork/src/components/landingpage.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 export default function Landingpage() {
     const [Counter, setCounter] = useState(0)
+    const [product, setProduct] = useState([])
     console.log("I m rendering in landing page compinent")
     let a = 10;
 
@@ -13,6 +14,7 @@ export default function Landingpage() {
             try {
                 let response = await axios.get('https://dummyjson.com/products');
                 console.log(response.data)
+                setProduct([response.data.products])
 
             } catch (error) {
                 console.log(error)
@@ -25,6 +27,7 @@ export default function Landingpage() {
     }, [])
 
     console.log("I m getting rendered in landing page component")
+    console.log('the products are', product)
     // [] Dependency array
     // case1 : Dependency is empty: useffect runs only single time when page renders
     // case2 : dependency array has some state or props: whenever the states and props changes, your useeffect changes that many times
