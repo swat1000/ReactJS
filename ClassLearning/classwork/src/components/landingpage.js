230621a867b05b877136d18cf4e0ab64fd2f4137
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-06-03 18:54:37 +0530

    WIP: Use effect api calls

--- ClassLearning/classwork/src/components/landingpage.js
+++ ClassLearning/classwork/src/components/landingpage.js
@@ -1,27 +1,38 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 // use effect
 
 export default function Landingpage() {
-    const[Counter,setCounter] = useState(0)
+    const [Counter, setCounter] = useState(0)
     console.log("I m rendering in landing page compinent")
-    let a= 10;
+    let a = 10;
 
     useEffect(() => {
         console.log("I m getting printed from useeffect body")
+        const apicalling = async () => {
+            try {
+                let response = await axios.get('https://dummyjson.com/products');
+                console.log(response.data)
 
-        // let body = document.querySelector(".header");
-        // console.log(body);
-    }, [Counter])
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        apicalling();
+
+
+    }, [])
 
     console.log("I m getting rendered in landing page component")
-     // [] Dependency array
-     // case1 : Dependency is empty: useffect runs only single time when page renders
-     // case2 : dependency array has some state or props: whenever the states and props changes, your useeffect changes that many times
+    // [] Dependency array
+    // case1 : Dependency is empty: useffect runs only single time when page renders
+    // case2 : dependency array has some state or props: whenever the states and props changes, your useeffect changes that many times
     return (
-    <div className='header'>
-        {/* in line calling of function in a single line */}
-        <button onClick={() => setCounter(Counter+1)}>Increment</button>
-        {Counter}
-    </div>
-  )
+        <div className='header'>
+            {/* in line calling of function in a single line */}
+            <button onClick={() => setCounter(Counter + 1)}>Increment</button>
+            {Counter}
+        </div>
+    )
 }
