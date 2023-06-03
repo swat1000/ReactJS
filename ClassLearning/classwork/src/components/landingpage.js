42932254008a8b3a15bfcb1130e4ca4b2c967c13
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-06-03 19:54:46 +0530

    Card structure using axios and api calling

--- ClassLearning/classwork/src/components/landingpage.js
+++ ClassLearning/classwork/src/components/landingpage.js
@@ -1,41 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-// use effect
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Landingpage() {
-    const [Counter, setCounter] = useState(0)
-    const [product, setProduct] = useState([])
-    console.log("I m rendering in landing page compinent")
-    let a = 10;
+export default function LandingPage() {
+  const [product, setProduct] = useState([]);
 
-    useEffect(() => {
-        console.log("I m getting printed from useeffect body")
-        const apicalling = async () => {
-            try {
-                let response = await axios.get('https://dummyjson.com/products');
-                console.log(response.data)
-                setProduct([response.data.products])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/products');
+        setProduct(response.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-            } catch (error) {
-                console.log(error)
-            }
-        }
+    fetchData();
+  }, []);
 
-        apicalling();
-
-
-    }, [])
-
-    console.log("I m getting rendered in landing page component")
-    console.log('the products are', product)
-    // [] Dependency array
-    // case1 : Dependency is empty: useffect runs only single time when page renders
-    // case2 : dependency array has some state or props: whenever the states and props changes, your useeffect changes that many times
-    return (
-        <div className='header'>
-            {/* in line calling of function in a single line */}
-            <button onClick={() => setCounter(Counter + 1)}>Increment</button>
-            {Counter}
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="row">
+        {product.length !== 0 ? (
+          product.map((element) => (
+            <div className="col-md-4 mb-4" key={element.id}>
+              <div className="card">
+                <img src={element.thumbnail} className="card-img-top" alt="Product Thumbnail" />
+                <div className="card-body">
+                  <h5 className="card-title">{element.title}</h5>
+                  <p className="card-text">{element.description}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Brand: {element.brand}</li>
+                  <li className="list-group-item">Category: {element.category}</li>
+                  <li className="list-group-item">Price: ${element.price}</li>
+                  <li className="list-group-item">Rating: {element.rating}</li>
+                  <li className="list-group-item">Stock: {element.stock}</li>
+                  <li className="list-group-item">Discount: {element.discountPercentage}%</li>
+                </ul>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No Data Available</div>
+        )}
+      </div>
+    </div>
+  );
 }
